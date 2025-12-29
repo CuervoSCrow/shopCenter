a71bfb4724929ebc613787748308355412d06259
Cuervo <vidalcuervo@gmail.com>
2025-12-29 12:45:08 -0600

    Leccion 12 Relaciones entre entidades @OneToMany

--- src/main/java/crow/example/shopcenter/entity/Local.java
+++ src/main/java/crow/example/shopcenter/entity/Local.java
@@ -3,6 +3,8 @@ package crow.example.shopcenter.entity;
 import jakarta.persistence.*;
 import lombok.*;
 
+import java.util.List;
+
 @Entity
 @Data
 @NoArgsConstructor
@@ -27,11 +29,22 @@ public class Local {
 
     @OneToOne(
             cascade = CascadeType.PERSIST,
-            fetch = FetchType.EAGER
+            fetch = FetchType.EAGER,
+            optional = false
     )
     @JoinColumn(
             name = "manager_id",
             referencedColumnName = "managerId"
     )
     private Manager manager;
+
+    @OneToMany(
+            cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER
+    )
+    @JoinColumn(
+            name = "local_id",
+            referencedColumnName = "localId"
+    )
+    private List<Order> orderList;
 }
--- src/test/java/crow/example/shopcenter/repository/LocalRepositoryTest.java
+++ src/test/java/crow/example/shopcenter/repository/LocalRepositoryTest.java
@@ -2,6 +2,7 @@ package crow.example.shopcenter.repository;
 
 import crow.example.shopcenter.entity.Local;
 import crow.example.shopcenter.entity.Manager;
+import crow.example.shopcenter.entity.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -36,6 +37,46 @@ class LocalRepositoryTest {
     public void findByAllLocalsTest(){
         List<Local> locals = localRepository.findAll();
         System.out.println("locals = " + locals);
+    }
+
+    @Test
+    public void saveLocalWithOrdersTest(){
+        Manager manager = Manager.builder()
+                .firstName("Bob")
+                .lastName("Smith")
+                .build();
+
+        Order order = Order.builder()
+                .description("Entrada Pelicual 1, Sala 5")
+                .price(6.50)
+                .build();
+
+        Order order2 = Order.builder()
+                .description("Entrada Pelicula 2, Sala 3")
+                .price(8.00)
+                .build();
+
+        Local local =
+                Local.builder()
+                        .name("Cinema")
+                        .floor("Third Floor")
+                        .manager(manager)
+                        .orderList(List.of(order, order2))
+                        .build();
+
+        localRepository.save(local);
+    }
+
+    @Test
+    public void findAllLocalsWithOrdersTest(){
+        List<Local> locals = localRepository.findAll();
+        for(Local local : locals){
+            System.out.println("local = " + local);
+            System.out.println("Orders:");
+            for(Order order : local.getOrderList()){
+                System.out.println("\t" + order);
+            }
+        }
     }
 
 }

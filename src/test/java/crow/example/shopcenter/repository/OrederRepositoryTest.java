e8823f023c648c10702a6b2459fd93d079f4b7c6
Cuervo <vidalcuervo@gmail.com>
2025-12-29 15:58:50 -0600

    Leccion 15 Paginando y Ordenamiento de Pedidos

--- src/test/java/crow/example/shopcenter/repository/OrederRepositoryTest.java
+++ src/test/java/crow/example/shopcenter/repository/OrederRepositoryTest.java
@@ -6,6 +6,11 @@ import crow.example.shopcenter.entity.Order;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -35,6 +40,61 @@ class OrederRepositoryTest {
                 .build();
 
         orderRepository.save(order);
+    }
+
+    @Test
+    public void findAllOrdersPaginationTest(){
+       Pageable firstPageWithThreeElements =
+               PageRequest.of(0,3);
+
+       List<Order> orderList = orderRepository.findAll(firstPageWithThreeElements).getContent();
+       long totalElements = orderRepository.findAll(firstPageWithThreeElements).getTotalElements();
+       long totalPages = orderRepository.findAll(firstPageWithThreeElements).getTotalPages();
+        System.out.println("totalElements = " + totalElements);
+        System.out.println("totalPages = " + totalPages);
+       for (Order order : orderList) {
+           System.out.println("order = " + order);
+       }
+
+    }
+
+    @Test
+    public void fillAllOrdersWithSortingTest(){
+        Pageable sortByPrice =
+                PageRequest.of(
+                        0,
+                        3,
+                        Sort.by("price")
+                );
+        Pageable sortByPriceDesc =
+                PageRequest.of(
+                        0,
+                        3,
+                        Sort.by("price").descending()
+                );
+
+        List<Order> orderList =
+                orderRepository.findAll(sortByPriceDesc).getContent();
+
+        for (Order order : orderList) {
+            System.out.println("order = " + order);
+        }
+    }
+
+    @Test
+    public void findAllOrdersByDescriptionContainingIgnoreCaseTest(){
+        Pageable firstPageWithTwoElements =
+                PageRequest.of(0,5);
+
+        List<Order> orderList =
+                orderRepository.findByDescriptionContainingIgnoreCase(
+                        "En",
+                        firstPageWithTwoElements
+                ).getContent();
+
+        for (Order order : orderList) {
+            System.out.println("order = " + order);
+        }
     }
 
 }
